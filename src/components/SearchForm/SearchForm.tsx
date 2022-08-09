5105f5fc642a46d645fbbd7718ca28eca283bb31
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-08-09 23:41:40 +0300

    react micro project

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -7,25 +7,17 @@ import {IQuery} from "../../interfaces";
 import {searchValidator} from "../../validators";
 
 import css from "./SearchForm.module.css"
-import {useAppDispatch, useAppSelector} from "../../hooks";
-import {movieActions} from "../../redux";
 
 const SearchForm: FC = () => {
     const {register, reset, handleSubmit, formState: {errors}} = useForm({mode: "onTouched",
         resolver: joiResolver(searchValidator)
     })
 
-    // const {searchmovies} = useAppSelector(state => state.movieReducer);
-    // const dispatch = useAppDispatch();
-
     const navigate = useNavigate()
 
     const search = (query: IQuery) => {
         navigate(`?query=${query.query}`)
         reset()
-        // if (searchmovies.length === 0) {
-        //     dispatch(movieActions.setNoResults({noResults: true}))
-        // }
     }
 
     return (
